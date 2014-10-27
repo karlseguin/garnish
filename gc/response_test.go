b27b229f286980ab2d533f7f6ba56ebb6a9df7ce
Karl Seguin <karl@openmymind.io>
2014-10-27 21:26:36 +0700

    response tests

---
+++ gc/response_test.go
@@ -0,0 +1,27 @@
+package gc
+
+import (
+	. "github.com/karlseguin/expect"
+	"testing"
+)
+
+type ResponseTests struct{}
+
+func Test_Response(t *testing.T) {
+	Expectify(new(ResponseTests), t)
+}
+
+func (r *ResponseTests) Empty() {
+	res := Empty(9001)
+	Expect(res.Status()).To.Equal(9001)
+	Expect(res.Body()).To.Equal(nil)
+	Expect(res.Header().Len()).To.Equal(0)
+}
+
+func (r *ResponseTests) Fatal() {
+	res := Fatal("a message")
+	Expect(res.Status()).To.Equal(500)
+	Expect(res.Body()).To.Equal(nil)
+	Expect(res.Header().Len()).To.Equal(0)
+	Expect(res.(*FatalResponse).Err).ToEqual("a message")
+}
