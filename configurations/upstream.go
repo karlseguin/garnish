b81558a2c5c53ab574d138066dbedd719e12fe27
Karl Seguin <karl@openmymind.io>
2014-10-30 19:49:42 +0700

    always use the dns cache

--- configurations/upstream.go
+++ configurations/upstream.go
@@ -130,10 +130,6 @@ func (u *Upstream) Build(runtime *gc.Runtime) *gc.Upstream {
 			//strip out the :80 which Go adds
 			return net.Dial("unix", address[:len(address)-3])
 		}
-	} else if u.dnsDuration == 0 {
-		transport.Dial = func(network, address string) (net.Conn, error) {
-			return net.Dial(network, address)
-		}
 	} else {
 		transport.Dial = func(network, address string) (net.Conn, error) {
 			separator := strings.LastIndex(address, ":")
