dccd9c6540d1165553037cccf593d829df221357
Karl Seguin <karl@openmymind.io>
2015-02-25 10:39:20 +0700

    cleaned up example

--- example/main.go
+++ example/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/karlseguin/garnish"
 	"github.com/karlseguin/garnish/gc"
 	"time"
@@ -13,15 +12,12 @@ func main() {
 	config.Stats().FileName("stats.json").Slow(time.Millisecond * 100)
 	config.Cache().Grace(time.Minute).PurgeHandler(PurgeHandler)
 	config.Upstream("test").Address("http://localhost:3000").KeepAlive(8)
-	config.Route("users").Get("/v1/queue").Upstream("test").CacheTTL(time.Minute)
+	config.Route("users").Get("/v1/users").Upstream("test").CacheTTL(time.Minute)
 	runtime, err := config.Build()
-	fmt.Println(runtime.Cache.Load("cache.save"))
 	if err != nil {
 		panic(err)
 	}
-	go garnish.Start(runtime)
-	time.Sleep(time.Second * 10)
-	fmt.Println(runtime.Cache.Save("cache.save", 10, time.Second*10))
+	garnish.Start(runtime)
 }
 
 func HydrateLoader(fragment gc.ReferenceFragment) []byte {
